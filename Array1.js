8bf635e896d1cece3d5a24b670b32024a627d79d
The Glass Corner <ksimons@aggies.ncat.edu>
2022-02-21 21:32:59 -0500

    Array

--- Array1.js
+++ Array1.js
@@ -1,4 +1,4 @@
-const arry = [0, 1, 2, 3, 1, 3, 4, 1, 2, 3, 5];
+const arry = [0, 1, 2, 3, 1, 3, 4, 1, 2];
 
 const toFindDuplicates = arry => arry.filter((item, index) => arry.indexOf(item) !== index)
 const duplicateElements = toFindDuplicates(arry);
