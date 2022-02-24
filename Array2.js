3de19f89b9d3989e8ad4cd894ae82b02ddf17844
The Glass Corner <ksimons@aggies.ncat.edu>
2022-02-23 21:54:45 -0500

    array test

--- Array2.js
+++ Array2.js
@@ -1,6 +1,15 @@
 //Find the duplicates in a string and print them out.
 // const str = "The quick brown fox jumps over the lazy dog";
 
-const str = "The quick brown fox jumps over the lazy dog";
-str.charAt(0);
-console.log(str.charAt(0));
+const str = "The quick brown fox jumps,jumps over the lazy dog";
+
+const toFindDuplicates = str => {
+    const strArray = str.split("");
+    const dupeArray = [];
+    const toFindDuplicates = strArray.filter((item, index) => strArray.indexOf(item) !== index);
+    dupeArray.push(strArray[i]);
+    return dupeArray.join(" ");
+}
+
+console.log(toFindDuplicates(str));
+
